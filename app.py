fe91998998ce66b4fb84b3b9654afd3e55404ef0
nadajb24-meet <139688502+nadajb24-meet@users.noreply.github.com>
2023-08-01 19:29:47 +0300

    kjkjkjkjkj

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 import os
-from flask import Flask, render_template, request, jsonify
-import openai
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
@@ -23,62 +21,44 @@ auth = firebase.auth()
 db=firebase.database()
 
 
-openai.api_key = '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'
-
-@app.route('/start', methods=['POST', 'GET'])
-def start():
-    if request.method == 'POST':
-        try:
-            data = request.json
-            if data and 'message' in data:
-                user_message = data['message']
-                response = openai.Completion.create(
-                    engine="gpt-3.5-turbo",  # Use gpt-3.5-turbo engine or the correct Babbage engine name if available
-                    prompt=user_message,
-                    max_tokens=150,
-                    temperature=0.7
-                )
-                chatbot_response = response.choices[0].text.strip()
-                return jsonify({'message': chatbot_response})
-            else:
-                return jsonify({'message': 'Invalid data format. Expected JSON with "message" field.'}), 400
-        except Exception as e:
-            return jsonify({'message': str(e)}), 500  # Return error message and status code 500 for server errors
-    else:
-        return render_template('start.html')
 
 
 
 
 
-
-
-@app.route('/')
-def home():
-	return render_template('home.html')
-
-
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    error=''
+@app.route('/', methods=['GET','POST'])
+def index():
+    result=''
     if request.method=='POST':
+        num=int(request.form['num'])
+        students=int(num/1500)
+        books=int(num/2500)
+        result1= num
+        result2=students
+        info =[]
+        if num > 1500 and num < 2500:
+            info=["By donating this amount of money you are helping this amount of woman ",""]
+
+        elif num >= 2500 :
+            info=["This donation will cover the costs of ","students per year","the number of books units in a text book, or enrichment materials"]
+
+        elif num < 1500:
+            info = ["you can contrbute to hire more staff members to improve our lessons quality"]
+        return render_template('index.html', result1 = result1,result2=result2,info=info)
+    return render_template('index.html', result = None)
+
+
+
+@app.route('/donate', methods=['GET', 'POST'])
+def donate():
+    if request.method=='POST':
+        name=request.form['name']
         email=request.form['email']
-        password=request.form['password']
-        full_name=request.form['full_name']
-        username=request.form['username']
-        try:
-            login_session['user']=auth.create_user_with_email_and_password(email, password)
-            UID = login_session['user']['localId']
-            user={'email':email, 'password':password, 'full_name':full_name, 'username':username}
-            print("meow2")
-            db.child('Users').child(UID).set(user)
-            return redirect (url_for('home'))
-        except:
-            error='Authentication failed'
-            return render_template('signup.html')
-    else:
-        return render_template('signup.html')
+        message=request.form['message']
+        user={"name":name,"email":email,"message":message}
+        db.child('Users').push(user)
+        return redirect (url_for('index'))
+    return render_template("donate.html")
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
@@ -110,9 +90,6 @@ questions = [
     # Add more questions as needed
 ]
 
-@app.route('/quiz')
-def quiz():
-    return render_template('gameshiraz.html', questions=questions)
 
 @app.route('/submit', methods=['POST'])
 def submit():
